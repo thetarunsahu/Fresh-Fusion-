d6607435cae732ed1927c1a0427bcc106b2b6a7d
Tarun Kumar Sahu <tarunkumarsahu354@gmail.com>
2026-09-03 23:25:35 +0530

    Load automatic fruit identity dashboard styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
+import './auto.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
