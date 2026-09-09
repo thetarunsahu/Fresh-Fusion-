6db4ba7666d6dbdda17906684f09f2e20370dde6
Tarun Kumar Sahu <tarunkumarsahu354@gmail.com>
2026-09-09 11:27:15 +0530

    Boot FreshFusion through authenticated portal

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
-import './auto.css';
-import './validation.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PortalApp from "./portal/PortalApp.jsx";
+import "./styles.css";
+import "./auto.css";
+import "./validation.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PortalApp />
+  </React.StrictMode>,
+);
